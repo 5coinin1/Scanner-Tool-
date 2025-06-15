ad1b01b426e370448095a96785439d3c66af5469
5coinin1 <theworldendupwithsalt@gmail.com>
2025-06-15 19:13:14 +0700

    oops

---
+++ gui/scanner_gui.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from gui.main_scanner_window import NetworkScannerGUI
+
+def main():
+    app = QApplication(sys.argv)
+    window = NetworkScannerGUI()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
